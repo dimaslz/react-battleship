a771cf0e2123baea8305324450c042d954b58cc7
dimaslz <me@dimaslz.io>
2023-06-09 02:39:37 +0200

    test(hook): add test for actions

--- src/hooks/use-board.hook.test.ts
+++ src/hooks/use-board.hook.test.ts
@@ -1,25 +1,24 @@
 import { act, renderHook } from "@testing-library/react";
 
-import { BOATS, LETTERS, MAX_SCORES } from "@/constants";
-import { generateItems } from "@/methods";
+import { BOARD_SIZE, LETTERS, MAX_SCORES, PLAYER, SHOT_VALUE } from "@/constants";
+import items from "@/mocks/items.json";
 import { BOARD_BOX_ITEM, BOARD_ROW } from "@/types";
+import { randomNumber } from "@/utils";
 
 import useBoard from "./use-board.hook";
 
 describe("[Hooks]: useBoard", () => {
-	describe("default values", () => {
-		let expected: any;
+	let expected: any;
 
-		beforeAll(async () => {
-			const items = generateItems();
+	beforeAll(async () => {
+		expected = await act(async () => {
+			const { result } = await renderHook(() => useBoard(items));
 
-			expected = await act(async () => {
-				const { result } = await renderHook(() => useBoard(items));
-
-				return result;
-			});
+			return result;
 		});
+	});
 
+	describe("default values", () => {
 		test("`turn` should be 'null'", async () => {
 			const { turn } = expected.current;
 			expect(turn).toBe(null);
@@ -70,7 +69,7 @@ describe("[Hooks]: useBoard", () => {
 			expect(humanWins).toBe(false);
 		});
 
-		test(`max possible scores are ${BOATS}`, () => {
+		test(`max possible scores are ${MAX_SCORES}`, () => {
 			const { maxScores } = expected.current;
 
 			expect(maxScores).toBe(MAX_SCORES);
@@ -83,11 +82,70 @@ describe("[Hooks]: useBoard", () => {
 			expect(typeof setTurn).toBe("function");
 		});
 
-		test("'setTurn' should exists and is a function", () => {
+		test("'updateItems' should exists and is a function", () => {
 			const { updateItems } = expected.current;
 
 			expect(updateItems).toBeTruthy();
 			expect(typeof updateItems).toBe("function");
 		});
+	});
+
+	describe("actions", () => {
+		const box = randomNumber(1, BOARD_SIZE * BOARD_SIZE);
+		const date = Date.now();
+
+		test("update items", () => {
+			const { updateItems, items } = expected.current;
+
+			updateItems((prevItems: BOARD_BOX_ITEM[]) => {
+				return prevItems.map((item) => {
+					if (item.box === box) {
+						item.player[PLAYER.COMPUTER].shot = {
+							date,
+							value: SHOT_VALUE.TOUCH,
+						};
+					}
+
+					return item;
+				});
+			});
+
+			const item = items.find((_item: BOARD_BOX_ITEM) => _item.box === box);
+			expect(item.player).toStrictEqual({
+				computer: {
+					filled: false,
+					shot: {
+						date,
+						value: SHOT_VALUE.TOUCH,
+					},
+				},
+				human: {
+					filled: false,
+					shot: null,
+				},
+			});
+		});
+
+		test("board matrix is updated", () => {
+			const { board } = expected.current;
+
+			const row = Math.ceil(box / 10);
+			const item = board[row - 1].find((col: BOARD_BOX_ITEM) => col.box === box);
+
+			expect(item.player).toStrictEqual({
+				computer: {
+					filled: false,
+					shot: {
+						date,
+						value: SHOT_VALUE.TOUCH,
+					},
+				},
+				human: {
+					filled: false,
+					shot: null,
+				},
+			});
+		});
+
 	});
 });
