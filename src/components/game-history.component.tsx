ab11808ffd85c5f34fc935a851311ad3dc44eb03
dimaslz <me@dimaslz.io>
2023-06-09 16:50:56 +0200

    feat: add move counter in history list

--- src/components/game-history.component.tsx
+++ src/components/game-history.component.tsx
@@ -11,19 +11,22 @@ const GameHistory = ({ history }: Props) => {
 	return (
 		<div className='mt-7'>
 			{history.length ? (
-				<div className='w-auto overflow-y-scroll text-left space-y-2 h-[400px]'>
-					{history.map((historyItem, historyItemIndex) => {
-						const date = dayjs(historyItem.date).format("YYYY/MM/DD - HH:mm:ss");
-						const who = historyItem.who === PLAYER.COMPUTER ? "💻" : "🙋‍♂️";
-						const value = historyItem.value === SHOT_VALUE.WATER ? "💦" : "💥";
+				<div>
+					<div className="text-sm text-left">({history.length} moves)</div>
+					<div className='w-auto overflow-y-scroll text-left space-y-2 h-[400px] pt-2'>
+						{history.map((historyItem, historyItemIndex) => {
+							const date = dayjs(historyItem.date).format("YYYY/MM/DD - HH:mm:ss");
+							const who = historyItem.who === PLAYER.COMPUTER ? "💻" : "🙋‍♂️";
+							const value = historyItem.value === SHOT_VALUE.WATER ? "💦" : "💥";
 
-						return (
-							<div className='text-sm flex space-x-2' key={historyItemIndex}>
-								<div>{who} [{date}]:</div>
-								<div className='text-slate-600'>{historyItem.label} - {value}</div>
-							</div>
-						);
-					})}
+							return (
+								<div className='text-sm flex space-x-2' key={historyItemIndex}>
+									<div>{who} [{date}]:</div>
+									<div className='text-slate-600'>{historyItem.label} - {value}</div>
+								</div>
+							);
+						})}
+					</div>
 				</div>
 			) : (
 				<div className='w-full h-[420px] flex items-center justify-center text-slate-400'>
